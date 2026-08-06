8d366873b5eff9aa9a37bf6acaf51a78d6409dd4
jkhpyuginbk <53821467+Rashedrabea@users.noreply.github.com>
2026-08-06 21:40:38 +0300

    Add files via upload

---
+++ gitignore.sh
@@ -0,0 +1,14 @@
+# تهيئة Git
+git init
+
+# إضافة الملفات
+git add index.html README.md deploy.sh .gitignore
+
+# عمل commit
+git commit -m "🚀 نشر نظام راشد المحاسبي v5.0"
+
+# إضافة المستودع البعيد
+git remote add origin https://github.com/your-username/rashed-accounting.git
+
+# رفع الملفات
+git push -u origin main
